4b3925450e850b9e56133c0bfb9e487a6a20bcf3
macchifederico <federico.macchi@gmail.com>
2022-07-31 12:46:35 -0300

    Fix url api Educacion

--- portfolio/src/app/services/educacion.service.ts
+++ portfolio/src/app/services/educacion.service.ts
@@ -8,7 +8,7 @@ import { Educacion } from '../models/Educacion';
 })
 export class EducacionService {
 
-  eduURL = "https://fedemacchi-portfolio.herokuapp.com/educacion/"
+  eduURL = "https://fedemacchi-portfolio.herokuapp.com/api/educacion/"
 
   constructor(private http: HttpClient) { }
 
